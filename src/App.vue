7adb77a69071d3e3b5c04bb38f7572a157a91e47
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-05-18 20:44:24 +0900

    #1 チャット画面修正

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
         <!-- ログイン時にはフォームとログアウトボタンを表示 -->
         <div v-if="user.uid" key="login" class="d-flex align-center">
-          <v-avatar size="40" class="mr-4">
+          <v-avatar size="40" class="mr-3">
             <img :src="user.photoURL" />
           </v-avatar>
           <v-btn @click="doLogout" outlined>ログアウト</v-btn>
@@ -21,55 +21,53 @@
     <!--Firebase から取得したリストを描画（トランジション付き）-->
     <v-content>
       <v-container class="container">
-        <transition-group name="chat" tag="div" class="list content">
-          <div
-            v-for="{ key, name, image, message, time, chatUid } in chat"
-            :key="key"
-          >
-            <div v-if="(user.uid === chatUid)" class="d-flex justify-end mb-4">
-              <div>
-                <div class="body-2 text-end">{{ name }}</div>
-                <div class="d-flex align-end">
-                  <div class="overline mr-2">{{ time }}</div>
-                  <v-card outlined color="#f0ffe0" class="body-1 px-2 py-1">
-                    <nl2br tag="div" :text="message" />
-                  </v-card>
-                </div>
+        <div v-for="{ key, name, image, message, time, chatUid } in chat" :key="key">
+          <div v-if="(user.uid === chatUid)" class="d-flex justify-end mb-4 ml-auto chat-item">
+            <div>
+              <div class="body-2 text-end">{{ name }}</div>
+              <div class="d-flex align-end">
+                <div class="caption mr-2">{{ time }}</div>
+                <v-card outlined color="#f0ffe0" class="body-1 px-2 py-1 message">
+                  <nl2br tag="div" :text="message" />
+                </v-card>
               </div>
-              <v-avatar size="40" class="ml-2">
-                <img :src="image" width="40" height="40" />
-              </v-avatar>
             </div>
-            <div v-else class="d-flex mb-4">
-              <v-avatar size="40" class="mr-2">
-                <img :src="image" width="40" height="40" />
-              </v-avatar>
-              <div>
-                <div class="body-2">{{ name }}</div>
-                <div class="d-flex align-end">
-                  <v-card outlined color="#f0ffe0" class="body-1 px-2 py-1">
-                    <nl2br tag="div" :text="message" />
-                  </v-card>
-                  <div class="overline ml-2">{{ time }}</div>
-                </div>
+            <v-avatar size="40" class="mt-1 ml-3">
+              <img :src="image" width="40" height="40" />
+            </v-avatar>
+          </div>
+          <div v-else class="d-flex mb-4 chat-item">
+            <v-avatar size="40" class="mt-1 mr-4">
+              <img :src="image" width="40" height="40" />
+            </v-avatar>
+            <div>
+              <div class="body-2">{{ name }}</div>
+              <div class="d-flex align-end">
+                <v-card outlined color="#f0ffe0" class="body-1 px-2 py-1">
+                  <nl2br tag="div" :text="message" />
+                </v-card>
+                <div class="caption ml-2">{{ time }}</div>
               </div>
             </div>
           </div>
-        </transition-group>
+        </div>
       </v-container>
     </v-content>
     <v-footer app color="#f5f5f5" height="120">
       <v-spacer></v-spacer>
       <!-- 入力フォーム -->
       <v-form action @submit.prevent="doSend" class="d-flex align-center container">
-        <v-text-field
+        <v-textarea
           v-model="input"
           :disabled="!user.uid"
-          @keydown.enter.exact.prevent="doSend"
+          @keydown.shift.enter.exact.prevent="doSend"
           class="mr-2"
-          clearable
-        ></v-text-field>
-        <v-btn type="submit" :disabled="!user.uid" color="primary" small>送信</v-btn>
+          outlined
+          no-resize
+          rows="2"
+          background-color="white"
+        ></v-textarea>
+        <v-btn type="submit" :disabled="!user.uid" dark color="#41b883">送信</v-btn>
       </v-form>
       <v-spacer></v-spacer>
     </v-footer>
@@ -88,7 +86,7 @@ export default {
     return {
       user: {}, // ユーザー情報
       chat: [], // 取得したメッセージを入れる配列
-      input: "", // 入力したメッセージ
+      input: "" // 入力したメッセージ
     };
   },
   created() {
@@ -114,6 +112,10 @@ export default {
     // ログアウト処理
     doLogout() {
       firebase.auth().signOut();
+    },
+    NewLine() {
+        // 改行処理の例
+        this.input = `${this.input}\n`;
     },
     // スクロール位置を一番下に移動
     scrollBottom() {
@@ -167,5 +169,12 @@ export default {
 .container {
   max-width: 700px !important;
   width: 100% !important;
+}
+.chat-item {
+  max-width: 75%;
+  width: 100%;
+}
+textarea {
+  overflow: hidden;
 }
 </style>
